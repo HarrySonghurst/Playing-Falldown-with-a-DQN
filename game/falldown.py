f0372d49a7cba160c003afe401653708a0eb227f
Harry Songhurst <harrysonghurst@gmail.com>
2016-11-30 00:04:01 +0000

    Turns out friction aint so great for rolling balls. Made a custom velocity function for the agent which dampens the x-velocity each step. Also added another velocity function for the agent to keep it static when the game starts. Platforms are now split up.

--- game/falldown.py
+++ game/falldown.py
@@ -8,7 +8,6 @@ import numpy as np
 
 
 height, width = 512, 384
-obstacle_spacing = 50
 
 # pygame initialisations
 pygame.init()
@@ -25,39 +24,44 @@ draw_options = pymunk.pygame_util.DrawOptions(screen)
 
 class Environment:
 
-    def __init__(self):
+    def __init__(self, platform_speed=80, platform_spacing=80, gap_size=50):
         self.init_agent()
         self.init_boundaries()
         self.running = True
-        self.hold_stationary = True
-        # init list of obstacles and create the first one
-        self.obstacles = []
-        self.create_new_obstacle()
+        self.hold_agent_stationary = True
+        # init list of platforms and create the first one
+        self.platform_spacing = platform_spacing
+        self.platform_speed = platform_speed
+        self.gap_size = gap_size
+        self.platforms = []
+        self.create_new_platform()
         pass
 
     def tick(self, action_to_take):
 
-        # initially, don't allow the agent to move, and hold the body still until the first obstacle
+        # initially, don't allow the agent to move, and hold the body still until the first platform
         # has passed the vertical midpoint of the screen.
         # take_action()
-        # if the last obstacle in the list of obstacles has passed an appropriate height, create_new_obstacle()
-        # remove_off_screen_obstacles()
+        # if the last platform in the list of platforms has passed an appropriate height, create_new_platform()
+        # remove_off_screen_platforms()
 
-        # don't allow the agent to move until the first obstacle has passed the y-midpoint
-        # if not self.hold_stationary:
-        #     self.check_game_begin_condition()
-        # if self.hold_stationary:
-        #     self.agent_body.position = (width/2, height/2)
-        #     self.agent_body.velocity = (0.0, 0.0)
+        # don't allow the agent to move until the first platform has passed the y-midpoint
+        if self.hold_agent_stationary:
+            self.check_game_begin_condition()
 
-        if self.obstacles[-1][0].position[1] > 50:
-            self.create_new_obstacle()
 
+        # create new platform when the lowest platform height > platform_spacing
+        if self.platforms[-1][0].position[1] > self.platform_spacing:
+            self.create_new_platform()
+            
+        self.remove_off_screen_platforms()
         self.take_action(action_to_take)
+
+        # update physics and draw to screen
         screen.fill(THECOLORS["white"])
         space.debug_draw(draw_options)
         space.step(1.0/60.0)
-        pygame.display.flip()
+        pygame.display.update()
         clock.tick(50)
         pygame.display.set_caption("Falldown (fps: " + str(clock.get_fps()) + ")")
 
@@ -77,55 +81,112 @@ class Environment:
         # action == [0, 1] : decrease agent x velocity
         # action == [1, 0] : increase agent x velocity
         if a == [0, 1]:
-            self.agent_body.apply_impulse_at_local_point((-10.0, 0.0), (0.0, 0.0))
+            self.agent_body.apply_impulse_at_local_point((-15.0, 0.0), (0.0, 0.0))
         elif a == [1, 0]:
-            self.agent_body.apply_impulse_at_local_point((10.0, 0.0), (0.0, 0.0))
+            self.agent_body.apply_impulse_at_local_point((15.0, 0.0), (0.0, 0.0))
         pass
 
     def get_reward(self):
         # check whether the agents y-position is less than or equal to its radius, if true,
         # return -100 (maybe -(total score)?).
         # check whether the y-coordinate of the top of the agent is greater than all the bottom
-        # y-coordinates of the obstacles in the list, also check if the 'passed' boolean on
-        # the obstacle is false, if it is, then return 10 and set the boolean true.
+        # y-coordinates of the platforms in the list, also check if the 'passed' boolean on
+        # the platform is false, if it is, then return 10 and set the boolean true.
         # else return -0.1 to encourage speed
         # returns tuple of (double reward, boolean terminal_status)
         pass
 
-    def create_new_obstacle(self):
-        # add new pymunk body to the space that overrides gravity, give it a constant velocity upwards, friction > 0.
-        # append the body to a list of obstacle bodies in a tuple with a 'passed' boolean set to false.
-        # A kinematic body is an hybrid body which is not affected by forces and collisions like
-        # a static body but can moved with a linear velocity like a dynamic body.
-        obstacle_body = pymunk.Body(mass=1, moment=1, body_type=pymunk.Body.KINEMATIC)
-        obstacle_body.position = (0.0, 0.0)
-        obstacle_shape = pymunk.Poly(obstacle_body, [(5.0,5.0),(5.0,10.0),(width-5, 5.0), (width-5, 10.0)])
-        # obstacle_shape = pymunk.Segment(obstacle_body, (5.0, ))
-        obstacle_shape.color = THECOLORS["black"]
-        obstacle_shape.friction = 0.5
-        obstacle_shape.elasticity = 0.1
-        obstacle_body._set_velocity_func(self.obstacle_velocity_function())
-        space.add(obstacle_body, obstacle_shape)
-        self.obstacles.append((obstacle_body, False))
+    def create_new_platform(self):
+        # add new platform with between 60% change gap in middle, 40% chance at either end.
+        if np.random.random() < 0.4:
+            if np.random.random() > 0.5:
+                platform_start = 4
+                platform_end = width - self.gap_size
+            else:
+                platform_start = self.gap_size
+                platform_end = width - 4
+            platform_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+            platform_body.position = (0.0, 0.0)
+            platform_shape = pymunk.Poly(platform_body, [(platform_start,0.0),
+                                                         (platform_start,5.0),
+                                                         (platform_end, 0.0),
+                                                         (platform_end, 5.0)])
+            platform_shape.color = THECOLORS["black"]
+            platform_shape.elasticity = 1
+            platform_body._set_velocity_func(self.platform_velocity_function())
+            space.add(platform_body, platform_shape)
+            self.platforms.append((platform_body, platform_shape, False))
+        else:
+            line_1_start = 4
+            line_1_end = np.random.randint(self.gap_size+4, width-(self.gap_size*2)-4)
+            line_2_start = line_1_end + self.gap_size
+            line_2_end = width-4
+            platform_1_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+            platform_1_body.position = (0.0, 0.0)
+            platform_1_shape = pymunk.Poly(platform_1_body, [(line_1_start,0.0),
+                                                         (line_1_start,5.0),
+                                                         (line_1_end, 0.0),
+                                                         (line_1_end, 5.0)])
+            platform_1_shape.color = THECOLORS["black"]
+            platform_1_shape.elasticity = 1
+            platform_1_body._set_velocity_func(self.platform_velocity_function())
+            platform_2_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+            platform_2_body.position = (0.0, 0.0)
+            platform_2_shape = pymunk.Poly(platform_2_body, [(line_2_start,0.0),
+                                                         (line_2_start,5.0),
+                                                         (line_2_end, 0.0),
+                                                         (line_2_end, 5.0)])
+            platform_2_shape.color = THECOLORS["black"]
+            platform_2_shape.elasticity = 1
+            platform_2_body._set_velocity_func(self.platform_velocity_function())
+            space.add(platform_1_body, platform_1_shape, platform_2_body, platform_2_shape)
+            self.platforms.append((platform_1_body, platform_1_shape, platform_2_body,
+                                   platform_2_shape, False))
         pass
 
-    # velocity integration function for the obstacles.
-    def obstacle_velocity_function(self):
+    def remove_off_screen_platforms(self):
+        # if bottom of platform y-coordinate is less than 0, remove it from the list and space.
+        if self.platforms[0][0].position[1] > height:
+            if len(self.platforms[0]) > 3:
+                space.remove(self.platforms[0][0], self.platforms[0][1],
+                             self.platforms[0][2], self.platforms[0][3])
+            else:
+                space.remove(self.platforms[0][0], self.platforms[0][1])
+            self.platforms.pop(0)
+        pass
+
+    def check_game_begin_condition(self):
+        # self.agent_body.position = (width/2, height/2)
+        if self.platforms[0][0].position[1] > 80:
+            self.agent_body._set_velocity_func(self.agent_velocity_function())
+            self.hold_agent_stationary = False
+        pass
+
+    # velocity integration function for the platforms.
+    def platform_velocity_function(self):
         def f(body, gravity, damping, dt):
-            body._set_velocity((0.0, 20))
+            body._set_velocity((0.0, self.platform_speed))
         return f
 
-    def remove_off_screen_obstacles(self):
-        # if bottom of obstacle y-coordinate is less than 0, remove it from the list and space.
-        pass
+    # agent velocity integration function to allow for x dampening (friction producing weird behaviour)
+    def agent_velocity_function(self):
+        def f(body, gravity, damping, dt):
+            body._set_velocity((body.velocity.x * 0.975, body.velocity.y+(gravity[1]*dt)))
+        return f
+
+    # to hold the agent stationary initially (v. bad to _set_velocity at each time step)
+    def agent_stationary_velocity_function(self):
+        def f(body, gravity, damping, dt):
+            body._set_velocity((0.0, 0.0))
+        return f
 
     def init_agent(self):
         self.agent_body = pymunk.Body(mass=1, moment=1)
         self.agent_body.position = (width/2, height/2)
         self.agent_shape = pymunk.Circle(self.agent_body, 15)
         self.agent_shape.color = THECOLORS["red"]
-        self.agent_shape.elasticity = 0.1
-        self.agent_shape.friction = 0.0
+        self.agent_shape.elasticity = 0.2
+        self.agent_body._set_velocity_func(self.agent_stationary_velocity_function())
         space.add(self.agent_body, self.agent_shape)
 
     def init_boundaries(self):
@@ -141,8 +202,6 @@ class Environment:
             wall.friction = 0.50
         space.add(walls)
 
-    def check_game_begin_condition(self):
-        pass
 
 if __name__ == "__main__":
     environment = Environment()
